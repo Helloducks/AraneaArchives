71c976486b2ae5ad4d234d1f0c13a2e8a890037d
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-05-31 14:16:23 -0400

    Create page added to home page but no DB connection

---
+++ AraneaArchives/Controllers/AddNoteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AraneaArchives.Controllers
+{
+    public class AddNoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ AraneaArchives/Models/Notes.cs
@@ -0,0 +1,9 @@
+﻿namespace AraneaArchives.Models
+{
+    public class Notes
+    {
+        public int NoteId { get; set; }
+        public string NoteName { get; set; }
+        public string NoteContents { get; set; }
+    }
+}
