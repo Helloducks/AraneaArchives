9205d79793128c5b9c604e8ea0b1cb79c5ece5e4
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-06-11 01:20:55 -0400

    just added some css, nothing much has changed from previous commit functionally

--- AraneaArchives/Controllers/HomeController.cs
+++ AraneaArchives/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace AraneaArchives.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
---
+++ AraneaArchives/Migrations/20230611042945_checkIfdatabasewillshowUp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AraneaArchives.Migrations
+{
+    /// <inheritdoc />
+    public partial class checkIfdatabasewillshowUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
