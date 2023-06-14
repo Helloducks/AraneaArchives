1d2ec31dc67004321756ceb9571ca16d21792b3e
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-06-13 22:28:35 -0400

    added some more to read controller

--- AraneaArchives/Controllers/ReadController.cs
+++ AraneaArchives/Controllers/ReadController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AraneaArchives.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AraneaArchives.Controllers
 {
     public class ReadController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ReadController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null || _context.Notes == null)
+            {
+                return NotFound();
+            }
+
+            var directories = _context.Notes.Where(n => n.DirectoryId == id).ToList();
+            if (directories == null)
+            {
+                return NotFound();
+            }
+            return View(directories);
+        }
+
+        public async Task<IActionResult> OpenPage(int? id)
+        {
+            if (id == null || _context.Notes == null)
+            {
+                return NotFound();
+            }
+
+            var notes = _context.Notes.FindAsync(id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+            return View(notes);
+        }
+
+
     }
 }
