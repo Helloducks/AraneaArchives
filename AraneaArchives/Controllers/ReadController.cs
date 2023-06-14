64ca2b210db757c07bdcaf8a4d7274ff07855b28
Bookofvoid <Saarath.Rathee@MyGeorgian.ca>
2023-06-13 22:17:07 -0400

    added readController

---
+++ AraneaArchives/Controllers/ReadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AraneaArchives.Controllers
+{
+    public class ReadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
